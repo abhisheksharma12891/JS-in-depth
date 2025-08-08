ad26fa76d72fbe1d2af63b165e0fc63e1608e95b
Abhishek Kr. Sharma <abhisheksharma12891@gmail.com>
2025-08-09 02:30:13 +0530

    day - 1.1

---
+++ Readme.md
@@ -0,0 +1,3 @@
+netscape browser -> approx 35 to 36 years ago
+var -> Function-scoped 
+let -> Block-scoped
---
+++ script.js
@@ -0,0 +1,61 @@
+console.log("Hello World!");
+console.log('Hello World!');
+console.log(`Hello World!`);
+console.log(14 + 11);
+console.log(typeof "qwerty");
+console.log(typeof 7);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof BigInt);
+console.log(typeof Symbol);
+console.log(parseInt("1234abcd"));
+
+
+let userFirstName = 'Abhishek'
+console.log(userFirstName);
+// console.log(typeof userFirstName);
+
+
+let userLastName = 'Sharma'
+console.log(userLastName);
+// console.log(typeof userLastName);
+
+
+let userFullName = userFirstName + " " + userLastName
+console.log(userFullName);
+// console.log(typeof userFullName);
+
+
+let userIntro = 'CSE STudent at PSIT'
+console.log(userIntro);
+// console.log(typeof userIntro);
+
+
+let userAge = 22
+console.log(userAge);
+// console.log(typeof userAge);
+
+
+let isMale = true
+console.log(isMale);
+// console.log(typeof isMale);
+
+
+let introLine = "Hi, my name is" + " " + userFirstName + " " + userLastName + " " + "and I am" + " " + userAge + " " + "years old."
+console.log(introLine);
+// console.log(typeof introLine);
+
+
+userFirstName = "Gullu"
+console.log(userFirstName)
+// console.log(typeof userFirstName)
+
+
+const dayInWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+console.log(dayInWeek)
+console.log(typeof dayInWeek)
+console.log(dayInWeek.length)
+console.log(typeof dayInWeek.length)
+
+
