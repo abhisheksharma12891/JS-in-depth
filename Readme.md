6a253a2235ff4729e2429fe5dec262624ff8351e
Abhishek Kr. Sharma <abhisheksharma12891@gmail.com>
2025-08-09 21:20:05 +0530

    day - 1.2

--- Readme.md
+++ Readme.md
@@ -1,3 +1,9 @@
 netscape browser -> approx 35 to 36 years ago
 var -> Function-scoped 
 let -> Block-scoped
+memory creation (can't pause) -> put type undefined -> code execute (can pause) ->
+Defer ->
+Debugger ->
+Temporal dead zone ->
+Template literals ->
+
--- script.js
+++ script.js
@@ -1,61 +1,103 @@
-console.log("Hello World!");
-console.log('Hello World!');
-console.log(`Hello World!`);
-console.log(14 + 11);
-console.log(typeof "qwerty");
-console.log(typeof 7);
-console.log(typeof true);
-console.log(typeof undefined);
-console.log(typeof null);
-console.log(typeof BigInt);
-console.log(typeof Symbol);
-console.log(parseInt("1234abcd"));
+// console.log("Hello World!");
+// console.log('Hello World!');
+// console.log(`Hello World!`);
+// console.log(14 + 11);
+// console.log(typeof "qwerty");
+// console.log(typeof 7);
+// console.log(typeof true);
+// console.log(typeof undefined);
+// console.log(typeof null);
+// console.log(typeof BigInt);
+// console.log(typeof Symbol);
+// console.log(parseInt("1234abcd"));
 
 
-let userFirstName = 'Abhishek'
-console.log(userFirstName);
+// let userFirstName = 'Abhishek'
+// console.log(userFirstName);
 // console.log(typeof userFirstName);
 
 
-let userLastName = 'Sharma'
-console.log(userLastName);
+// let userLastName = 'Sharma'
+// console.log(userLastName);
 // console.log(typeof userLastName);
 
 
-let userFullName = userFirstName + " " + userLastName
-console.log(userFullName);
+// let userFullName = userFirstName + " " + userLastName
+// console.log(userFullName);
 // console.log(typeof userFullName);
 
 
-let userIntro = 'CSE STudent at PSIT'
-console.log(userIntro);
+// let userIntro = 'CSE STudent at PSIT'
+// console.log(userIntro);
 // console.log(typeof userIntro);
 
 
-let userAge = 22
-console.log(userAge);
+// let userAge = 22
+// console.log(userAge);
 // console.log(typeof userAge);
 
 
-let isMale = true
-console.log(isMale);
+// let isMale = true
+// console.log(isMale);
 // console.log(typeof isMale);
 
 
-let introLine = "Hi, my name is" + " " + userFirstName + " " + userLastName + " " + "and I am" + " " + userAge + " " + "years old."
-console.log(introLine);
+// let introLine = "Hi, my name is" + " " + userFirstName + " " + userLastName + " " + "and I am" + " " + userAge + " " + "years old."
+// console.log(introLine);
 // console.log(typeof introLine);
 
 
-userFirstName = "Gullu"
-console.log(userFirstName)
+// userFirstName = "Gullu"
+// console.log(userFirstName)
 // console.log(typeof userFirstName)
 
 
-const dayInWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-console.log(dayInWeek)
-console.log(typeof dayInWeek)
-console.log(dayInWeek.length)
-console.log(typeof dayInWeek.length)
+// const dayInWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+// console.log(dayInWeek)
+// console.log(typeof dayInWeek)
+// console.log(dayInWeek.length)
+// console.log(typeof dayInWeek.length)
+
+
+// alert("this is a alert")
+// confirm("this is a confirm")
+// const userInput = prompt("this is a prompt")
+// console.log(userInput)
+// const isConfirmed = confirm("this is a confirm")
+// console.log(isConfirmed)
+
+
+const message = "Hello, World!";
+console.log(message)
+console.log(message[3])
+console.log(message .length)
+console.log(message .toUpperCase())
+console.log(message .toLowerCase())
+console.log(message .charAt(0))
+console.log(message .charCodeAt(0))
+console.log(message .includes("World"))
+console.log(message .indexOf("World"))
+console.log(message .lastIndexOf("World"))
+console.log(message .slice(7, 12))
+console.log(message .split(" "))
+console.log(message .concat())
+console.log(message .trim())
+console.log(message .padStart(10, "Hello"))
+console.log(message .padEnd(10, "World"))
+console.log(message .repeat(3))
+console.log(message .replace("World", "Universe"))
+console.log(message .replaceAll("W", "U"))
+console.log(message .startsWith("Hello"))
+console.log(message .endsWith("World"))
+console.log(message .includes("World", 7)) // not supported in all browsers
+console.log(message .includes("World", 7, 10)) // not supported in all browsers
+console.log(message .includes("World", 7, 10, 15)) // not
+console.log(message .includes("World", 7, 10, 15, 20))
+console.log(message .includes("World", 75))
+
+
+
+
+
 
 
